1642f090c1e968c77c1fb9c5102df54522443dc0
Iza <mrs.master.of.disaster@gmail.com>
2023-08-04 11:52:50 +0200

    Update zgadnijLiczbe.js

--- zgadnijLiczbe.js
+++ zgadnijLiczbe.js
@@ -6,49 +6,43 @@ function zgadnijLiczbe() {
 
     let inputData = parseInt(prompt("Spróbuj odgadnąć liczbę od 1 do 10. Masz 5 prób!\nWprowadź liczbę:"));
         
-    for (i=1; i<6; i++) {
+    for (let i=1; i<6; i++) {
 
-        if (inputData === null) {
-            break;
-        }
-
-        else {
-
-            if (Number.isInteger(inputData)) {
-
-                if (randomNumber === inputData && i<6) {
-                    alert(`Gratulacje! Odgadłaś za ${i} razem!`);
-                    if (confirm("Jeszcze raz?")) {
-                        zgadnijLiczbe();
-                    }
-                    break;
+        if (!isNaN(inputData)){
+            
+            if (randomNumber === inputData && i<6) {
+                alert(`Gratulacje! Odgadłaś za ${i} razem!`);
+                if (confirm("Jeszcze raz?")) {
+                    zgadnijLiczbe();
                 }
-
-                else if (inputData < randomNumber && i<5) {
-                    inputData = parseInt(prompt(`Niestety! Spróbuj ponownie.\nPodana przez Ciebie liczba jest mniejsza o ${randomNumber-inputData}.\nPróba ${i+1}/5.`));
-                }
-                
-                else if (inputData > randomNumber && i<5) {
-                    inputData = parseInt(prompt(`Niestety! Spróbuj ponownie.\nPodana przez Ciebie liczba jest większa o ${inputData-randomNumber}.\nPróba ${i+1}/5.`));
-                }
-
-                else if (i === 5){
-                    alert("Niestety - przegrałaś.");
-                    if (confirm("Jeszcze raz?")) {
-                        zgadnijLiczbe();
-                    }
-                    break;
-                }
-
-                else break;
+                break;
             }
 
-        else {
-            inputData = parseInt(prompt("Wprowadź liczbę, nie tekst"));
-            i--;
-        }
-    }
+            else if (inputData < randomNumber && i<5) {
+                inputData = parseInt(prompt(`Niestety! Spróbuj ponownie.\nPodana przez Ciebie liczba jest mniejsza o ${randomNumber-inputData}.\nPróba ${i+1}/5.`));
+            }
+                
+            else if (inputData > randomNumber && i<5) {
+                inputData = parseInt(prompt(`Niestety! Spróbuj ponownie.\nPodana przez Ciebie liczba jest większa o ${inputData-randomNumber}.\nPróba ${i+1}/5.`));
+            }
 
+            else if (i === 5){
+                alert("Niestety - przegrałaś.");
+                if (confirm("Jeszcze raz?")) {
+                    zgadnijLiczbe();
+                }
+                break;
+            }
+
+            else break;
+        }
+
+        else if (confirm("Wprowadź liczbę - nie tekst.")){
+                i--;
+                inputData = parseInt(prompt(`Wprowadź liczbę.\nPróba ${i+1}/5.`));
+        }
+
+        else break;
     }
 }
 
